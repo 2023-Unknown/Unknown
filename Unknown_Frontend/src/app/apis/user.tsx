7ebffd199cefa342b16f962c1f00be0a60933241
ksy990628 <ksy990628@gmail.com>
2023-10-19 00:39:45 +0900

    chore: prettier 적용

--- Unknown_Frontend/src/app/apis/user.tsx
+++ Unknown_Frontend/src/app/apis/user.tsx
@@ -6,7 +6,10 @@ export const signup = async (formData: {
 	useremail: string;
 	userpassword: string;
 }) => {
-	const response = await axios.post('http://localhost:8080/api/v1/users/create', formData);
+	const response = await axios.post(
+		'http://localhost:8080/api/v1/users/create',
+		formData,
+	);
 	const { username, useremail, userpassword } = response.data;
 
 	// localStorage.setItem("token", token); // 로컬 스토리지에 토큰 저장
