a0e1c840f496e22b0fd0ddbec15c0ef48582280e
ksy990628 <ksy990628@gmail.com>
2023-11-05 17:30:40 +0900

    feat: Spinner 흰색 지구탐색 추가

--- Unknown_Frontend/src/app/components/Spinner.tsx
+++ Unknown_Frontend/src/app/components/Spinner.tsx
@@ -10,11 +10,11 @@ export const Spinner = () => {
             https://loading.io/spinner/earth/-earth-globe-map-rotate */}
 
             {/* 2. 지구 자전 모형 (무료), 흰색*/}
-            <iframe src="https://giphy.com/embed/bEKmZEYwwNQFUkeTDv" width="480" height="416" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/stickers/bank-australia-responsible-banking-bankaust-bEKmZEYwwNQFUkeTDv">via GIPHY</a></p>
-            <Image src="/spinner_1.gif" alt="spinner" width="64" height="64" />
+            {/* <iframe src="https://giphy.com/embed/bEKmZEYwwNQFUkeTDv" width="480" height="416" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/stickers/bank-australia-responsible-banking-bankaust-bEKmZEYwwNQFUkeTDv">via GIPHY</a></p>*/}
+            <Image src="/spinner_1.gif" alt="spinner" width="64" height="64" /> 
 
             {/* 3. 지구 자전 모형 (무료), 지구색*/}
-            <iframe src="https://giphy.com/embed/0O5NwsKlbgN2EF3q6x" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/world-earth-globe-0O5NwsKlbgN2EF3q6x">via GIPHY</a></p>
+            {/* <iframe src="https://giphy.com/embed/0O5NwsKlbgN2EF3q6x" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/world-earth-globe-0O5NwsKlbgN2EF3q6x">via GIPHY</a></p> */}
             <Image src="/spinner_2.gif" alt="spinner" width="64" height="64" />
 
             {/* 4. 일반 스피너(무료), 색상 지정 가능
@@ -22,6 +22,10 @@ export const Spinner = () => {
 
             {/* 5. 일반 스피너(무료), 색상 지정 가능 
             https://schum123.github.io/svelte-loading-spinners/?ref=madewithsvelte.com*/}
+
+            {/* 6. 흰색 지구 탐색 */}
+            {/* <iframe src="https://giphy.com/embed/rDLR9Ycj4OIeUkhr0N" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/stickers/minbz-bz-wereld-wereldbol-rDLR9Ycj4OIeUkhr0N">via GIPHY</a></p> */}
+            <Image src="/spinner_3.gif" alt="spinner" width="64" height="64" /> 
             </div>
     );
 }
