38b3e5ee2d0cc4ee0e727632e837e7a9ec028b39
ksy990628 <ksy990628@gmail.com>
2023-11-05 19:40:36 +0900

    feat: Resize spinner

--- Unknown_Frontend/src/app/components/Spinner.tsx
+++ Unknown_Frontend/src/app/components/Spinner.tsx
@@ -3,18 +3,16 @@ import Image from 'next/image';
 export const Spinner = () => {
 	return (
 		<div>
-			<h3>Finding your chat mate...</h3>
-
 			{/* 1. 지구 자전 모형 (유료)
             https://loading.io/spinner/earth/-earth-globe-map-rotate */}
 
 			{/* 2. 지구 자전 모형 (무료), 흰색*/}
 			{/* <iframe src="https://giphy.com/embed/bEKmZEYwwNQFUkeTDv" width="480" height="416" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/stickers/bank-australia-responsible-banking-bankaust-bEKmZEYwwNQFUkeTDv">via GIPHY</a></p>*/}
-			<Image src='/spinner_1.gif' alt='spinner' width='64' height='64' />
+			<Image src='/spinner_1.gif' alt='spinner' width='120' height='120' />
 
 			{/* 3. 지구 자전 모형 (무료), 지구색*/}
 			{/* <iframe src="https://giphy.com/embed/0O5NwsKlbgN2EF3q6x" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/world-earth-globe-0O5NwsKlbgN2EF3q6x">via GIPHY</a></p> */}
-			<Image src='/spinner_2.gif' alt='spinner' width='64' height='64' />
+			<Image src='/spinner_2.gif' alt='spinner' width='120' height='120' />
 
 			{/* 4. 일반 스피너(무료), 색상 지정 가능
             https://loading.io/spinner/spin/-spinner-ball-circle-rotate-rosary-loader-ajax*/}
@@ -24,7 +22,7 @@ export const Spinner = () => {
 
 			{/* 6. 흰색 지구 탐색 */}
 			{/* <iframe src="https://giphy.com/embed/rDLR9Ycj4OIeUkhr0N" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/stickers/minbz-bz-wereld-wereldbol-rDLR9Ycj4OIeUkhr0N">via GIPHY</a></p> */}
-			<Image src='/spinner_3.gif' alt='spinner' width='64' height='64' />
+			<Image src='/spinner_3.gif' alt='spinner' width='120' height='120' />
 		</div>
 	);
 };
