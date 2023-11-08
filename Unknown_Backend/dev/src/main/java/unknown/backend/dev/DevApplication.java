9d905147f32c676fe93a638066d04c2403c8f068
heesane <tees3359@gmail.com>
2023-11-09 04:38:00 +0900

    chore : ServletComponentScan

--- Unknown_Backend/dev/src/main/java/unknown/backend/dev/DevApplication.java
+++ Unknown_Backend/dev/src/main/java/unknown/backend/dev/DevApplication.java
@@ -2,10 +2,12 @@ package unknown.backend.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ServletComponentScan
 public class DevApplication {
 
 	public static void main(String[] args) {
