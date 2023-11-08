557560d32d535481c602c6fc59ccd5cb332ae974
heesane <tees3359@gmail.com>
2023-11-09 04:40:41 +0900

    feat : Login Request DTO

---
+++ Unknown_Backend/dev/src/main/java/unknown/backend/dev/dto/LoginRequest.java
@@ -0,0 +1,14 @@
+package unknown.backend.dev.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginRequest {
+    private String loginEmail;
+    private String password;
+
+}
