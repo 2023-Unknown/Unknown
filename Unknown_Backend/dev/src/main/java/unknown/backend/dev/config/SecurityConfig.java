f69cdb57122f3ac14f731ffa0309c119dd74b2de
heesane <tees3359@gmail.com>
2023-11-09 04:39:51 +0900

    feat : SecurityConfig

---
+++ Unknown_Backend/dev/src/main/java/unknown/backend/dev/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package unknown.backend.dev.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import unknown.backend.dev.domain.UserRole;
+import unknown.backend.dev.filter.JwtTokenFilter;
+import unknown.backend.dev.service.UserService;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final UserService userService;
+    private final static String secretKey = "my-secret-key-123123";
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers("/jwt-login/info").authenticated()
+                .antMatchers("/jwt-login/admin/**").hasAuthority(UserRole.ROLE_ADMIN.name())
+                .and().build();
+    }
+}
