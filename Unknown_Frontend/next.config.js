79416a36cab4a52e9c4ce1e894baf39be69dcb1a
ksy990628 <ksy990628@gmail.com>
2023-11-11 17:39:35 +0900

    chore: Change url path

--- Unknown_Frontend/next.config.js
+++ Unknown_Frontend/next.config.js
@@ -8,8 +8,7 @@ const nextConfig = {
 		return [
 			{
 				source: '/:path*',
-				destination: 'http://localhost:8080/:path*',
-				// destination: 'http://localhost:8080/api/v1/:path*',
+				destination: 'http://localhost:8080/api/v1/:path*',
 			},
 		];
 	},
