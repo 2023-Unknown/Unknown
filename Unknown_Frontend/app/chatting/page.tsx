a075edefe54beb67953bd5eed5fe2fd931575d4a
heesane <tees3359@gmail.com>
2023-11-29 01:35:10 +0900

    feat : useState를 통해 WebSocket 객체 전달

--- Unknown_Frontend/app/chatting/page.tsx
+++ Unknown_Frontend/app/chatting/page.tsx
@@ -7,15 +7,51 @@ import ChatPrompt from '@/components/ChatPrompt';
 import { Spinner } from '../../components/Spinner';
 import { userToken } from '../../states/user';
 import { getChatroom } from '@/apis/chat';
+import { Client } from '@stomp/stompjs';
+import { WebSocket } from 'ws';
 
 export default function Main() {
 	const [roomId, setRoomId] = useState(0);
+	const [ws, setWs] = useState<Client | null>(null);
 	const tokenState = useRecoilValue(userToken);
+	let promiseRoomId = "";
 
 	const enterChat = async () => {
 		try {
-			const enterChat = await getChatroom(tokenState).then((res) => {
+			await getChatroom(tokenState).then((res) => {
+				console.log("res: " + res);
+				promiseRoomId = res;
 				setRoomId(res);
+
+				const WebSocketClient = new Client({
+					brokerURL: 'ws://localhost:8080/ws/chat',
+					debug: function (str) {
+						console.log(str);
+					},
+					reconnectDelay: 5000,
+					heartbeatIncoming: 4000,
+					heartbeatOutgoing: 4000,
+
+					onConnect: () => {
+						console.log('onConnect');
+						// 123부분에 roomId를 넣으면 됩니다.
+						WebSocketClient.subscribe('/topic/chat/room/123', (message) => {
+							console.log(`Received Message : ${message.body}`);
+						});
+						WebSocketClient.publish({
+							destination: '/app/chat/message',
+							body: JSON.stringify({
+								"type": "TALK",
+								"sender": "unknown",
+								"message": "qerweqer",
+								"roomId": "123"
+							})
+						});
+					}
+
+				});
+				WebSocketClient.activate();
+				setWs(WebSocketClient);
 			});
 		} catch (error) {
 			console.error(error); // 이 부분은 에러처리를 원하는 방식으로 변경하실 수 있습니다.
@@ -27,12 +63,13 @@ export default function Main() {
 	// -> 랜덤 채팅방 매칭 후, 채팅방 컴포넌트 뜰 것
 	useEffect(() => {
 		enterChat();
-	}, []);
+		console.log("roomId: " + promiseRoomId);
+	}, [userToken]);
 
 	return (
 		<div>
 			<Logo />
-			{roomId === 0 ? <Spinner /> : <ChatPrompt />}
+			{roomId === 0 ? <Spinner /> : <ChatPrompt ws={ws} />}
 		</div>
 	);
 }
