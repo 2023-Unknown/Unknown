107c14ad027c6c73f162b7d525f101b8f6e47112
ksy990628 <ksy990628@gmail.com>
2023-12-03 16:53:07 +0900

    feat: Add othermessage chat ui from subscription

--- Unknown_Frontend/components/ChatPrompt.tsx
+++ Unknown_Frontend/components/ChatPrompt.tsx
@@ -2,20 +2,38 @@
 
 import React, { useState, useRef, useEffect } from 'react';
 import { WebSocketClient } from '../config/websocket';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+
+interface IMessage {
+	user: string;
+	message: string;
+}
 
 export default function ChatPrompt(room: string) {
-	const [message, setMessage] = useState<string>('');
-	const [messages, setMessages] = useState<string[]>([]);
-	const [omessages, setOMessages] = useState<string[]>([]);
+	const router = useRouter();
+
+	const [message, setMessage] = useState<string>(''); // 보낼 메세지 (1개)
+	const [chat, setChat] = useState<IMessage[]>([]); // 전체 메세지들
 	const chatMessagesRef = useRef<HTMLDivElement | null>(null);
 	const chatInputRef = useRef<HTMLInputElement>(null);
 
+	useEffect(() => {
+		scrollToBottom();
+	}, [chat]);
+
+	// 스크롤을 항상 가장 아래로 이동
+	const scrollToBottom = () => {
+		if (chatMessagesRef.current) {
+			chatMessagesRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
+		}
+	};
+
+	// WebSocket 연결
 	WebSocketClient.onConnect = () => {
-		WebSocketClient.subscribe('/topic/chat/room/123', (message) => {
+		WebSocketClient.subscribe('/topic/chat/room/123', (message: string) => {
 			// Received Message : {"type":"TALK","roomId":"123","sender":"heesane","message":"hi"}
-			let msg = JSON.parse(message.body); // String -> Json으로 변환
-			setOMessages([...omessages, msg.message]);
-			console.log(`${msg.sender} : ${msg.message}`);
+			handleReceivedMessage(message);
 		});
 	};
 	WebSocketClient.activate();
@@ -26,69 +44,76 @@ export default function ChatPrompt(room: string) {
 
 	const handleSubmit = (e: React.FormEvent) => {
 		e.preventDefault();
-		if (message.trim() !== '') {
-			// 추가된 부분 11.29
-			WebSocketClient?.publish({
-				destination: '/app/chat/message',
-				body: JSON.stringify({
-					type: 'TALK',
-					sender: 'heesane',
-					message: message,
-					roomId: '123',
-				}),
-			});
+		if (message.trim() === '') return;
 
-			setMessages([...messages, message]);
-			setMessage('');
-			// 스크롤을 항상 가장 아래로 이동
-			scrollToBottom();
-		}
+		WebSocketClient?.publish({
+			destination: '/app/chat/message',
+			body: JSON.stringify({
+				type: 'TALK',
+				sender: 'DefaultUser',
+				message: message,
+				roomId: '123',
+			}),
+		});
+		setMessage('');
 	};
 
-	// 스크롤을 항상 가장 아래로 이동
-	const scrollToBottom = () => {
-		if (chatMessagesRef.current) {
-			chatMessagesRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
-		}
+	const handleReceivedMessage = (message: string) => {
+		let data = JSON.parse(message.body); // String -> Json으로 변환
+		let msg = {
+			user: data.sender,
+			message: data.message,
+		};
+		chat.push(msg);
+		setChat([...chat]);
 	};
 
-	useEffect(() => {
-		// 메시지가 추가될 때 스크롤을 맨 아래로 이동
-		scrollToBottom();
-	}, [messages]);
+	const reportUser = () => {
+		/*신고버튼*/
+	};
+
+	const exitRoom = () => {
+		router.push('/');
+	};
 
 	return (
-		<div className='ChatBox'>
-			<div className='ChatMessages' ref={chatMessagesRef}>
-				{' '}
-				{/* 수정된 부분 */}
-				{messages.map((msg, index) => (
-					<div key={index} className='Message'>
-						{msg}
-					</div>
-				))}{' '}
-				{omessages.map((msg, index) => (
-					<div key={index} className='Message'>
-						{msg}
-					</div>
-				))}{' '}
-				{/* 수정된 부분 */}
+		<>
+			<div className='ToolBox'>
+				<button onClick={reportUser} className='ReportButton'>
+					<Image src='/report.png' alt='report' width='35' height='30' />
+				</button>
+				<button onClick={exitRoom} className='ExitButton'>
+					<Image src='/exit.png' alt='exit' width='50' height='80' />
+				</button>
 			</div>
-			<div className='ChatInputBox'>
-				<form onSubmit={handleSubmit}>
+			<div className='ChatBox'>
+				<div className='ChatMessages' ref={chatMessagesRef}>
+					{' '}
+					{chat.map((chat, index) => {
+						let css = chat.user === 'DefaultUser' ? 'MyMessage' : 'OtherMessage';
+						return (
+							<div key={index} className={css}>
+								{chat.message}
+							</div>
+						);
+					})}{' '}
+				</div>
+			</div>
+			<form onSubmit={handleSubmit}>
+				<div className='ChatInputBox'>
 					<input
 						ref={chatInputRef}
 						className='ChatInput'
 						type='text'
 						value={message}
 						onChange={handleInputChange}
-						placeholder='Type your message'
+						placeholder='   Type your message'
 					/>
-					<button className='ChatButton' type='submit'>
-						Send
-					</button>
-				</form>
-			</div>
-		</div>
+				</div>
+				<button className='ChatButton' type='submit'>
+					<Image src='/sendMessage.png' alt='send' width='40' height='40' />
+				</button>
+			</form>
+		</>
 	);
 }
